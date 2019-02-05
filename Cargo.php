b1ef261438d3a1a6d294b750f0d616dfc5f657ad
Pgarcia18 <47335637+Pgarcia18@users.noreply.github.com>
2019-02-04 20:26:32 -0400

    Add files via upload

--- Cargo.php
+++ Cargo.php
@@ -53,9 +53,12 @@ $link = Conectarse();
                 <li><a href="inicio.php"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
                 <li class="active"><a href="pedidos.php"><em class="fa fa-calendar">&nbsp;</em> Pedidos</a></li>
                 <li><a href="entregas.php"><em class="fa fa-bar-chart">&nbsp;</em> Entregas</a></li>
+                <li><a href="IniciarReparacion.php"><em class="fa fa-toggle-off">&nbsp;</em> Inicio de Reparacion</a></li>
                 <li><a href="reparacion.php"><em class="fa fa-toggle-off">&nbsp;</em> Reparacion</a></li>
                 <li><a href="traslado.php"><em class="fa fa-clone">&nbsp;</em> Traslados</a></li>
                 <li><a href="cargo.php"><em class="fa fa-clone">&nbsp;</em> Cargos</a></li>
+                 <li><a href="Verificar.php"><em class="fa fa-toggle-off">&nbsp;</em> Verificacion de Orden</a></li>
+                 <li><a href="traslado.php"><em class="fa fa-toggle-off">&nbsp;</em> Orden traslado</a></li>
                 <li class="parent ">
                     <a data-toggle="collapse" href="#sub-item-1">
                         <em class="fa fa-navicon">&nbsp;</em> Parametros <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
@@ -191,7 +194,7 @@ $link = Conectarse();
                  <div class="col-md-6">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            Listado de Personas
+                            Listado de Cargos
                             <span class="pull-right clickable panel-toggle panel-button-tab-left"><em class="fa fa-toggle-up"></em></span>
                         </div>
                         <table class="table table-responsive table-striped">
